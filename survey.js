bff36cd355cc1c82314410417b1d9b4ac16bca49
Moneylyst <131757834+Moneylyst@users.noreply.github.com>
2023-05-20 12:00:17 +0530

    Add files via upload

---
+++ survey.js
@@ -0,0 +1,34 @@
+// Created by Yuvraj Singh Sandhu (Inactive)
+
+
+// JavaScript code made by Yuvraj Singh Sandhu
+
+function search_animal() { 
+
+    let input = document.getElementById('searchbar').value 
+
+    input=input.toLowerCase(); 
+
+    let x = document.getElementsByClassName('animals'); 
+
+      
+
+    for (i = 0; i < x.length; i++) {  
+
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+
+            x[i].style.display="none"; 
+
+        } 
+
+        else { 
+
+            x[i].style.display="list-item";                  
+
+        } 
+
+    } 
+
+} 
+
+
